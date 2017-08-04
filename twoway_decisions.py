e70615645815adb6edd108c87f516ae194e2ed75
kdestasio <kdestasio@gmail.com>
2017-08-04 11:34:51 -0700

    Practice writing a script with two way decisions and user input.

---
+++ twoway_decisions.py
@@ -0,0 +1,12 @@
+# Two-way decisions with else
+
+print('Is x bigger than 2?')
+x = input('What value should x have?\n')
+x = int(x)
+
+if x > 2 :
+	print('Bigger')
+else : 
+	print('Smaller')
+
+print('All done')
