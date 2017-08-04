a77e4725f9e89f1c3be1e7ba91c724c7dd255908
kdestasio <kdestasio@gmail.com>
2017-08-04 11:40:33 -0700

    Allow user to provide input for x values.

--- conditional_steps.py
+++ conditional_steps.py
@@ -1,22 +1,37 @@
 # Toy code to illustrate conditional steps
 
-print('Script 1')
-x = 5
+print('Is x smaller than 10?')
+x = input('What value should x have?\n')
+x = int(x)
+
 if x < 10:
     print('Smaller')
 if x > 20:
     print('Bigger')
-
 print('Finis')
 
+#########
 print('')
-print('Script 2')
+#########
+
+print('Is x bigger than 2?')
+x = input('What value should x have?\n')
+x = int(x)
+
 if x > 2 :
     print('Bigger than 2')
     print('Still bigger')
 print('Done with 2')
 
-for i in range(5) :
+#########
+print('')
+#########
+
+print('Are numbers in range x greater than 2?')
+x = input('What value should x have?\n')
+x = int(x)
+
+for i in range(x) :
     print(i)
     if i > 2 :
         print('Bigger than 2')
