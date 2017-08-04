01c94f8653d0a72e4d5d2b3d22f5ed6c08bf2631
kdestasio <kdestasio@gmail.com>
2017-08-04 11:26:58 -0700

    Add a second script to illustrate conditional code and to practice blocks of code.

--- conditional_steps.py
+++ conditional_steps.py
@@ -1,9 +1,24 @@
 # Toy code to illustrate conditional steps
 
+print('Script 1')
 x = 5
 if x < 10:
-	print('Smaller')
+    print('Smaller')
 if x > 20:
-	print('Bigger')
+    print('Bigger')
 
 print('Finis')
+
+print('')
+print('Script 2')
+if x > 2 :
+    print('Bigger than 2')
+    print('Still bigger')
+print('Done with 2')
+
+for i in range(5) :
+    print(i)
+    if i > 2 :
+        print('Bigger than 2')
+    print('Done with i', i)
+print('All done')
