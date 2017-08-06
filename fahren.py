2d627ce95796ea81c845400bb3f583b8de444eee
kdestasio <kdestasio@gmail.com>
2017-08-06 13:07:56 -0700

    Practice with input

---
+++ fahren.py
@@ -0,0 +1,8 @@
+# Convert Farenheit to Celsius
+
+inp = input('Enter Farenheit Temperature: ')
+fahr = float(inp)
+cel = (fahr - 32.0) * 5.0 / 9.0
+print(cel)
+
+# Code: http://www.py4e.com/code3/fahren.py
