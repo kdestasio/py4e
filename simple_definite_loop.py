479c0cb33299b16dbff309c6011bfbf3a5950128
kdestasio <kdestasio@gmail.com>
2017-08-06 14:16:33 -0700

    Practice simple definite loops.

---
+++ simple_definite_loop.py
@@ -0,0 +1,12 @@
+# A simple definite loop
+
+## integers
+for i in [5, 4, 3, 2, 1] :
+	print(i)
+print('Blastoff! It\'s party time! And we all live in a fascist nation!')
+
+## strings
+friends = ['Joseph', 'Glen', 'Sally']
+for friend in friends :
+	print('Happy New Year,', friend)
+print('Done!')
